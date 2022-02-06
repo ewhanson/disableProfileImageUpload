10d914b2848811c68f615f788448075254e1705e
Erik Hanson <erik@erikhanson.dev>
2022-02-06 12:27:33 -0800

    Update copyright year

--- DisableProfileImageUploadPlugin.inc.php
+++ DisableProfileImageUploadPlugin.inc.php
@@ -3,8 +3,8 @@
 /**
  * @file plugins/generic/disableProfileImageUpload/DisableProfileImageUploadPlugin.inc.php
  *
- * Copyright (c) 2014-2017 Simon Fraser University
- * Copyright (c) 2003-2017 John Willinsky
+ * Copyright (c) 2014-2022 Simon Fraser University
+ * Copyright (c) 2003-2022 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class DisableProfileImageUploadPlugin
--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 /**
  * @file plugins/generic/disableProfileIMageUpload/index.php
  *
- * Copyright (c) 2014-2017 Simon Fraser University
- * Copyright (c) 2003-2017 John Willinsky
+ * Copyright (c) 2014-2022 Simon Fraser University
+ * Copyright (c) 2003-2022 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_generic_disableprofileimageupload
